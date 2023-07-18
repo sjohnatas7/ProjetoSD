214681e8751e74a66fcb0d6bf871621370fb6039
sjohnatas7 <johnatas@usp.br>
2023-07-18 11:08:25 -0300

    bdc-completo

--- simulacoes/bcd_controlador.v
+++ simulacoes/bcd_controlador.v
@@ -1,6 +1,6 @@
 `include "simulacoes/src/bcd_controlador_3digitos.v"
 
-module bcd_tb;
+module bcd_controlador;
 	reg ck, inicio, Vint_z;
 	wire enb_3, ld, qs1, qs2, qs3;
 	wire rst_s, ch_zr, ch_ref, ch_vm;
@@ -9,7 +9,7 @@ module bcd_tb;
 	wire [6:0] sgm2;
 
   // Instancia um bcd_controlador_3digitos e ja fornece dados iniciais
-	bcd_controlador_3digitos counter(.ck(ck), .rst_s(1'b0), .ld(1'b1), .enb_0(1'b1),
+	bcd_controlador_3digitos contador(.ck(ck), .rst_s(1'b0), .ld(1'b1), .enb_0(1'b1),
 								.sgm0(sgm0), .sgm1(sgm1), .sgm2(sgm2),
 								.enb_3(enb_3));
   // Gera clock
@@ -19,8 +19,8 @@ module bcd_tb;
   // Dados para a simulação
   initial begin
     $dumpfile("simulacoes/vcd/bcd.vcd");
-	$dumpvars(0, bcd_tb);
-    	ck <= 0;
+	$dumpvars(0, bcd_controlador);
+    	ck <= 1;
 		#6000
     $finish;
   end
